f9927dec47848d2600957630d3cbb7597aae679c
Nandy-tech <nandy5.tech@gmail.com>
2026-07-02 12:16:51 +0530

    Hide Tags nav item from Clerks (matches view-tags gate)

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -51,8 +51,10 @@ $nav = array_filter([
                 ? ['name' => 'Groups', 'route' => 'groups.index',
                    'icon' => 'M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10']
                 : null,
-            ['name' => 'Tags', 'route' => 'tags.index',
-             'icon' => 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z'],
+            $isManagerPlus
+                ? ['name' => 'Tags', 'route' => 'tags.index',
+                   'icon' => 'M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z']
+                : null,
             $isManagerPlus
                 ? ['name' => 'Reminders', 'route' => 'reminders.index',
                    'icon' => 'M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9']
